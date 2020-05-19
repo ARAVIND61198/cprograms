9afc0c2c4116a69527188918bf217348f2ca4278
ARAVIND JAGADEESAN <40851141+ARAVIND61198@users.noreply.github.com>
2020-05-19 10:55:55 +0530

    Add files via upload

---
+++ 1St_week/word_suggest/os.h
@@ -0,0 +1,20 @@
+#if _WIN32
+	#include<conio.h>
+	#define left 75
+	#define right 77
+	#define interuptor 0xE0
+	void ignore_function(){
+	}
+#elif __linux__
+	#define left 68
+	#define right 67
+	#define interuptor 27
+	#include"linux_header.h"
+	void ignore_function(){
+		char c = getch();
+	}
+#else
+	void system(){
+		printf("system not defined");
+	}
+#endif
